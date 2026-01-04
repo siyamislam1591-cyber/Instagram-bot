a5d0e5a847cc060c4da4f89376cdd2ec23f95c12
siyamislam1591-cyber <siyamislam1591@gmail.com>
2026-01-04 18:17:20 +0600

    Update Logger.js

--- core/Logger.js
+++ core/Logger.js
@@ -1,1 +1,26 @@
+/**
+ * Simple Logger for Instagram Bot
+ * You can use log.info(), log.error(), log.warn()
+ */
 
+const chalk = require("chalk"); // Optional, for colored console logs
+
+const log = {
+  info: (message) => {
+    console.log(chalk.blue("[INFO]"), message);
+  },
+
+  warn: (message) => {
+    console.warn(chalk.yellow("[WARN]"), message);
+  },
+
+  error: (message) => {
+    console.error(chalk.red("[ERROR]"), message);
+  },
+
+  debug: (message) => {
+    console.log(chalk.gray("[DEBUG]"), message);
+  },
+};
+
+module.exports = log;
